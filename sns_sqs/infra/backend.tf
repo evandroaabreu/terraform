38ba3996295c2e28e2b51ac7eb1a1745914a65c4
Evandro Araujo de Abreu <evandroaabreu@gmail.com>
2025-03-12 09:03:57 -0300

    Update backend.tf

--- sns_sqs/infra/backend.tf
+++ sns_sqs/infra/backend.tf
@@ -1,8 +1,25 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.65.0"
+    }
+  }
   backend "s3" {
-    bucket         = "meu-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket = "fsv1-terraform-state"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "aula_vpc" {
+
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "aula_vpc"
   }
 }
